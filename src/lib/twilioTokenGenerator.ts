419bf2c85b6a0e89a0fad7e3c30b618f456b7475
s76771246 <s76771246@gmail.com>
2025-09-09 12:14:06 +0530

    Update twilioTokenGenerator.ts
    ded

--- src/lib/twilioTokenGenerator.ts
+++ src/lib/twilioTokenGenerator.ts
@@ -3,7 +3,7 @@ const TWILIO_CONFIG = {
   account_sid: "AC61be89f63333b748858f9671feef26d5",  // Replace with your Account SID
   api_key_sid: "SK1f80046aafe5ba42d3230b7d00c8e11f",  // Replace with your API Key SID  
   api_key_secret: "kjc3hdMZ3zf1CwJZReAStBZQZHkMIV1w",  // Replace with your API Secret
-  twiml_app_sid: "ff"  // Replace with your TwiML App SID
+  twiml_app_sid: "APa5fee892ac98fdfb8fd8d0ba4eb07b4a"  // Replace with your TwiML App SID
 };
 
 // Identity for the browser user (no spaces, only alphanumeric + underscore)
